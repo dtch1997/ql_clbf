fc5a1e99becd62db29b217f5769f60d290fea8c4
Daniel Tan <dtch1997@users.noreply.github.com>
2023-05-02 16:30:01 +0100

    Add Gymnasium Robotics safety env

--- ql_clbf/envs/registration.py
+++ ql_clbf/envs/registration.py
@@ -1,10 +1,21 @@
 from gym.envs.registration import register
 
+
+from gymnasium.envs.registration import register as register_gymnasium
+from gymnasium_robotics.envs.maze import maps
+
 register(
     'SafeCartPole-v1',
     entry_point='ql_clbf.envs.safe_cartpole:SafeCartPoleEnv',
     max_episode_steps=500,
     reward_threshold=475.0,
+)
+
+register(
+    'SafePendulum-v1',
+    entry_point='ql_clbf.envs.safe_pendulum:SafePendulumEnv',
+    max_episode_steps=200,
+    reward_threshold=1.0,
 )
 
 register(
@@ -40,4 +51,15 @@ register(
     entry_point='ql_clbf.envs.decomposed_cartpole:CartPoleDEnv',
     max_episode_steps=500,
     reward_threshold=475.0,
+)
+
+register_gymnasium(
+    id='SafetyPointMaze_Open-v3',
+    entry_point='ql_clbf.envs.safety_point_maze:SafetyPointMazeEnv',
+    max_episode_steps=500,
+    reward_threshold=0.0,
+    kwargs={
+        "reward_type": 'sparse',
+        'maze_map': maps.OPEN,
+    },
 )
