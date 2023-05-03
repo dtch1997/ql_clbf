ae080e7a8c42bfe58d35d15603538f7050bfa927
Daniel Tan <dtch1997@users.noreply.github.com>
2023-05-03 16:55:07 +0100

    Add cart pole with diverse reset conditions

--- ql_clbf/envs/registration.py
+++ ql_clbf/envs/registration.py
@@ -13,6 +13,13 @@ register(
 )
 
 register(
+    'DiverseCArtPole-v1',
+    entry_point='ql_clbf.envs.safe_cartpole:DiverseCartPoleEnv',
+    max_episode_steps=500,
+    reward_threshold=475.0,
+)
+
+register(
     'NegSafeCartPole-v1',
     entry_point='ql_clbf.envs.safe_cartpole:NegSafeCartPoleEnv',
     max_episode_steps=500,
--- ql_clbf/envs/safe_cartpole.py
+++ ql_clbf/envs/safe_cartpole.py
@@ -1,11 +1,31 @@
 import math
 import numpy as np
 
+from typing import Optional 
 from gym import logger
 from gym.envs.classic_control import CartPoleEnv
 from ql_clbf.envs.env_interface import SafetyEnv, EnvHParams
 from ql_clbf.utils.gym_utils import get_flattened_dim
 
+class DiverseCartPoleEnv(CartPoleEnv):
+    """ CartPoleEnv with more diverse initial states """
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        return_info: bool = False,
+        options: Optional[dict] = None,
+    ):
+        super().reset(seed=seed)
+        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        start_x = self.np_random.uniform(low=-2.0, high=2.0)
+        self.state[0] = start_x        
+        self.steps_beyond_done = None
+        if not return_info:
+            return np.array(self.state, dtype=np.float32)
+        else:
+            return np.array(self.state, dtype=np.float32), {}
+        
 class ModifiableCartPoleEnv(CartPoleEnv):
     """ CartPoleEnv
     
